160843c4239b609cbba7a7f208f8f4a9ba6b8ef0
Igor Kozyrev <ikozyrev@gmail.com>
2019-04-01 16:51:59 +0300

    debug 1

--- classes/App/Controller/Api.php
+++ classes/App/Controller/Api.php
@@ -1348,7 +1348,9 @@ class Api extends \App\Page {
                     transp->pntall->
                     where('LOC_TGT_TYPE_CD', 'RC')->
                    // where('or',array('ORG_TGT_ID',$org->id()))->
-                    find_all();
+                    //find_all()->
+                    query->query()[0];
+            die($pnts);
            // $pnts_shop = $this->pixie->orm->get('pntall')->
            //         where('ORG_TGT_ID', $org->id())->
            //         find_all();
