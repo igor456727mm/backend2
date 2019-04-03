226fbe080584be56f043c9f2ca77387ecc31540d
Igor Kozyrev <ikozyrev@gmail.com>
2019-04-03 14:36:07 +0300

    debug 2

--- classes/App/Model/org.php
+++ classes/App/Model/org.php
@@ -27,11 +27,11 @@ class Org extends \PHPixie\ORM\Model {
             $date_filter=" and LOC_PLAN_DTTM>='$date_from'";
         }
         if ($date_to!='') {
-            $date_filter=" and LOC_PLAN_DTTM<='$date_to'";
+            $date_filter=$date_filter." and LOC_PLAN_DTTM<='$date_to'";
         }
         $qry = "select * from glr_allpoints a where a.TRNSP_ID in (select TRNSP_ID from glr_allpoints a1 where a1.ORG_TGT_ID=$org_id) and (ORG_TGT_ID=$org_id or LOC_TGT_TYPE_CD='RC')".$date_filter;
         $res = $this->conn->execute($qry);
-        die($qry);
+        //die($qry);
         return $res;
     }
 
