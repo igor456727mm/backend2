7cb2da93f735094c65ae5c6bf41a11183925c933
Igor Kozyrev <ikozyrev@gmail.com>
2019-05-17 11:08:37 +0300

    db test

--- assets/config/db.php
+++ assets/config/db.php
@@ -7,12 +7,12 @@ return array(
 		'driver' => 'PDO',
 		
 		//'Connection' is required if you use the PDO driver
-		'connection'=>'mysql:host=95.214.62.239;dbname=ikozyr9w_dh',
+		'connection'=>'mysql:host=45.67.56.75;dbname=ikozyr9w_dh',
                // 'connection'=>'mysql:host=5.101.152.204;dbname=ikozyr9w_dh',
 		
 		// 'db' and 'host' are required if you use Mysql driver
 		'db' => 'ikozyr9w_dh',
 		//'host'=>'5.101.152.204'
-                'host'=>'95.214.62.239'
+                'host'=>'45.67.56.75'
 	)
 );
