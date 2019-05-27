1b254c7ad92fc2141c048a0df4700fa5b5dda747
Igor Kozyrev <ikozyrev@gmail.com>
2019-05-27 11:01:01 +0300

    db config

---
+++ assets/config/db.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+        'default' => array(
+                'user'=>'ikozyr9w_dh',
+                'password' => 'BAIkal99!',
+                'driver' => 'PDO',
+
+                //'Connection' is required if you use the PDO driver
+                'connection'=>'mysql:host=45.67.56.75;dbname=ikozyr9w_dh',
+               // 'connection'=>'mysql:host=5.101.152.204;dbname=ikozyr9w_dh',
+
+                // 'db' and 'host' are required if you use Mysql driver
+                'db' => 'ikozyr9w_dh',
+                //'host'=>'5.101.152.204'
+                'host'=>'45.67.56.75'
+        )
+);
